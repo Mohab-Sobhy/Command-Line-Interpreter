33cc2ea0c746db2c4023f3c5da55fb91fb10bbad
mohab <mohab_sobhy@outlook.com>
2024-10-29 23:03:18 +0300

    color prompt line

--- CommandLineInterpreter.java
+++ CommandLineInterpreter.java
@@ -4,7 +4,8 @@ public class CommandLineInterpreter {
     public static void start(){
         Scanner scanner = new Scanner(System.in);
         while(true){
-            System.out.print(Meta.getCurrentDir() + " : ");
+            String promptLine = "\u001B[32mUser@ComputerName\u001B[0m:\u001B[34m" + Meta.getCurrentDir() + "\u001B[0m$ ";
+            System.out.print(promptLine);
             String userInput = scanner.nextLine();
             Controller.executeCommand(userInput);
 //            if(CommandParser.isThereAPipe){   [Moved to Controller]
