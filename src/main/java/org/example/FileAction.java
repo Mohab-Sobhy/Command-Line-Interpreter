3403aace309cf7790ea71b9668fac45a2da6d27a
abdulla-En <158290886+abdulla-En@users.noreply.github.com>
2024-10-31 19:45:16 +0300

    Update FileAction.java
    now problem with function type solved

--- src/main/java/org/example/FileAction.java
+++ src/main/java/org/example/FileAction.java
@@ -4,121 +4,103 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
-public class FileAction
-{
-    public static void touch (Object input)
-    {
-        String [] filename_s = Factory.Convert_to_array(input);
-        assert(filename_s != null);
+public class FileAction {
+    public static void touch(ArrayList<String> input) {
+        if (input == null || input.isEmpty()) {
+            System.err.println("No files specified.");
+            return;
+        }
 
-        for(String filename : filename_s)
-        {
+        for (String filename : input) {
             File newfile = Factory.createForD(filename);
             try {
-                if(newfile.createNewFile())
-                    System.out.println("File: "+newfile.getName()+" is created successfully.");
+                if (newfile.createNewFile())
+                    System.out.println("File: " + newfile.getName() + " created successfully.");
                 else
-                    System.err.println("File: "+newfile.getName()+ " is already exist besfore.");
-
-            } catch (Exception e) {
-                System.err.println("error creating a file: "+newfile.getName());
+                    System.err.println("File: " + newfile.getName() + " already exists.");
+            } catch (IOException e) {
+                System.err.println("Error creating file: " + newfile.getName());
             }
         }
-
     }
 
-    public static void rm(Object input)
-    {
-        String [] filename_s = Factory.Convert_to_array(input);
-        assert(filename_s != null);
+    public static void rm(ArrayList<String> input) {
+        if (input == null || input.isEmpty()) {
+            System.err.println("No files specified.");
+            return;
+        }
 
-        for(String filename : filename_s)
-        {
+        for (String filename : input) {
             File file = Factory.createForD(filename);
-            if(file.exists())
-            {
-                if(file.delete())
-                {
-                    System.out.println("File: "+file.getName()+" deleted successfully");
+            if (file.exists()) {
+                if (file.delete())
+                    System.out.println("File: " + file.getName() + " deleted successfully.");
+                else
+                    System.err.println("Failed to delete file: " + file.getName());
+            } else
+                System.err.println("File: " + file.getName() + " does not exist.");
+        }
+    }
+
+    public static void cat(ArrayList<String> input) {
+        if (input == null || input.isEmpty()) {
+            System.err.println("No files specified.");
+            return;
+        }
+
+        for (String filename : input) {
+            File file = Factory.createForD(filename);
+            if (file.exists()) {
+                System.out.println("Content of file " + file.getName() + ":");
+                try {
+                    Files.lines(Path.of(file.getPath())).forEach(System.out::println);
+                } catch (IOException e) {
+                    System.err.println("Error reading file: " + file.getName());
                 }
-                else
-                    System.err.println("failed to delete the file: "+file.getName());
-            }
-            else
-                System.err.println("File: "+file.getName()+" is not exists already");
-        }
-
-    }
-
-    public static void cat(Object input) throws IOException
-    {
-        String[] filename_s = Factory.Convert_to_array(input);
-        assert(filename_s != null);
-
-        for(String filename : filename_s)
-        {
-            File file = Factory.createForD(filename);
-            if(file.exists())
-            {
-                System.out.println("Content of the file : "+file.getName()+" :");
-                Files.lines(Path.of(file.getPath())).forEach(System.out::println);
-            }
-            else
-                System.err.println("File: "+file.getName()+" is not exists .");
+            } else
+                System.err.println("File: " + file.getName() + " does not exist.");
         }
     }
 
-    public static void mv(Object input , String dir_or_file)
-    {
-        String[] filename_s = Factory.Convert_to_array(input);
-        assert(filename_s != null);
-
-        if(filename_s.length == 1)
-        {
-            File source_file = Factory.createForD(filename_s[0]);
-            File DestFile = Factory.createForD(dir_or_file);
-
-            if(source_file.exists())
-            {
-                if(source_file.renameTo(DestFile))
-                    System.out.println("file : "+source_file.getName() + "move to: "+DestFile.getPath());
-                else
-                    System.err.println("error to move: "+source_file.getName()+"to: "+DestFile.getPath());
-            }
-            else
-                System.err.println("File: "+source_file.getName()+" is not exists");
-            return ;
+    public static void mv(ArrayList<String> input, String dir_or_file) {
+        if (input == null || input.isEmpty()) {
+            System.err.println("No files specified.");
+            return;
         }
-        else
-        {
-            File Dir= Factory.createForD(dir_or_file);
-            if(!Dir.exists())
-                Dir.mkdirs();
-            else if(!Dir.isDirectory())
-            {
-                System.err.println("no Directory name : "+dir_or_file);
+
+        if (input.size() == 1) {
+            File source_file = Factory.createForD(input.get(0));
+            File destFile = Factory.createForD(dir_or_file);
+
+            if (source_file.exists()) {
+                if (source_file.renameTo(destFile))
+                    System.out.println("File: " + source_file.getName() + " moved to: " + destFile.getPath());
+                else
+                    System.err.println("Error moving file: " + source_file.getName() + " to: " + destFile.getPath());
+            } else
+                System.err.println("File: " + source_file.getName() + " does not exist.");
+        } else {
+            File dir = Factory.createForD(dir_or_file);
+            if (!dir.exists())
+                dir.mkdirs();
+            else if (!dir.isDirectory()) {
+                System.err.println("Destination is not a directory: " + dir_or_file);
                 return;
             }
 
-            for(String filename : filename_s)
-            {
+            for (String filename : input) {
                 File sourceFile = Factory.createForD(filename);
-                if(sourceFile.exists())
-                {
-                    File DestFile = new File(dir_or_file, sourceFile.getName());
-                    if(sourceFile.renameTo(DestFile))
-                        System.out.println("File: "+sourceFile.getName()+"moved  to Dir: "+Dir.getPath()+" successfully");
+                if (sourceFile.exists()) {
+                    File destFile = new File(dir, sourceFile.getName());
+                    if (sourceFile.renameTo(destFile))
+                        System.out.println("File: " + sourceFile.getName() + " moved to directory: " + dir.getPath() + " successfully.");
                     else
-                        System.err.println("error to move File: "+sourceFile.getName()+"to Dir: "+Dir.getPath());
-                }
-                else
-                {
-                    System.err.println("failed to move File: "+sourceFile.getName());
-                }
+                        System.err.println("Error moving file: " + sourceFile.getName() + " to directory: " + dir.getPath());
+                } else
+                    System.err.println("File: " + sourceFile.getName() + " does not exist.");
             }
         }
-
-
     }
-};
+}
