4fb3c672e72c8e688d78e14410b4bb30ca3347e9
mohab <mohab_sobhy@outlook.com>
2024-10-31 22:39:27 +0300

    final commits

--- src/main/java/org/example/CommandLineInterpreter.java
+++ src/main/java/org/example/CommandLineInterpreter.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class CommandLineInterpreter {
     public static void start(){
         Scanner scanner = new Scanner(System.in);
+        String promptLine;
         while(true){
-            String promptLine = "\u001B[32m \u001B[0m:\u001B[34m" + Meta.getCurrentDir() + "\u001B[0m$ ";
+            promptLine = "\u001B[32mUser@ComputerName\u001B[0m:\u001B[34m" + Meta.getCurrentDir() + "\u001B[0m$ ";
             System.out.print(promptLine);
             String userInput = scanner.nextLine();
             Controller.executeCommand(userInput);
-
-
         }
     }
 }
