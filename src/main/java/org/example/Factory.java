5cd7d5ea33bacc4e979a685a3444eed5a66e6bb0
abdulla-En <158290886+abdulla-En@users.noreply.github.com>
2024-10-31 19:31:51 +0300

    Update Factory.java Remove(convert_to_array)func

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -8,17 +8,4 @@ public class Factory
         String currDir= Meta.getCurrentDir();
         return new File(currDir+"/"+Dirname);
     }
-
-    public static String[] Convert_to_array(Object Input)
-    {
-        if(Input instanceof String)
-            return new String[]{(String) Input};
-        else if(Input instanceof String[])
-            return (String[])Input ;
-        else{
-            System.err.println("invailed input must be string or string[] only");
-            return null;
-        }
-
-    }
 };
